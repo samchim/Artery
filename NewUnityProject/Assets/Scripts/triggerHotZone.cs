5b5b1281939120abf21d25823ba65b917aaf2a89
Sam Chim <samchim@mindcluster.ai>
2020-10-20 23:30:25 +0800

    rename + add hotzone

---
+++ NewUnityProject/Assets/Scripts/triggerHotZone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerHotZone : MonoBehaviour
+{
+    public Material materialSafe;
+    public Material materialOut;
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        rend = GetComponent<Renderer>();
+        rend.sharedMaterial = materialSafe;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Object Enter trigger");
+        rend.sharedMaterial = materialOut;
+    }
+}
