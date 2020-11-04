ab2b81349d24121bc337e8c8f8d1d4b8ea4ac65a
Sam Chim <samchim@mindcluster.ai>
2020-11-04 17:19:17 +0800

    try commit

--- NewUnityProject/Assets/Scripts/moveRelativeToCamera.cs
+++ NewUnityProject/Assets/Scripts/moveRelativeToCamera.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class moveRelativeToCamera : MonoBehaviour
 {
     private Camera mainCamera;
-    private float CameraZDistance;
+    // private float CameraZDistance;
     private Vector3 ScreenPosition;
     private Vector3 NewWorldPosition;
     private Rigidbody rb;
@@ -15,15 +15,15 @@ public class moveRelativeToCamera : MonoBehaviour
     void Start()
     {
         mainCamera = Camera.main;
-        CameraZDistance =
-            mainCamera.WorldToScreenPoint(transform.position).z; //z axis of the game object for screen view
+        // CameraZDistance =
+        //     mainCamera.WorldToScreenPoint(transform.position).z; //z axis of the game object for screen view
         rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0f, Input.GetAxis("Vertical") * Time.deltaTime);
-        CameraZDistance = mainCamera.WorldToScreenPoint(transform.position).z;
+        // CameraZDistance = mainCamera.WorldToScreenPoint(transform.position).z;
     }
 
 
