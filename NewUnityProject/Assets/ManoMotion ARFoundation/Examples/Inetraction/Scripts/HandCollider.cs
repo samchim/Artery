86ca772a3f02855aebc36a9752e9c450dcb57e35
Sam Chim <samchim@mindcluster.ai>
2021-02-16 18:04:57 +0800

    trying linearWeightedMovingAverage

--- NewUnityProject/Assets/ManoMotion ARFoundation/Examples/Inetraction/Scripts/HandCollider.cs
+++ NewUnityProject/Assets/ManoMotion ARFoundation/Examples/Inetraction/Scripts/HandCollider.cs
@@ -20,18 +20,22 @@ public class HandCollider : MonoBehaviour
     }
     #endregion
     public int smoothingFrame;
+    public bool movingAverage;
+    public bool linearWeightedMovingAverage;
 
     private TrackingInfo tracking;
     public Vector3 currentPosition;
     public Vector3 smoothedPostion;
     private List<Collider> collidings;
-    private Queue<Vector3> smoothingBuffer;
 
+    private Queue<Vector3> smoothingBuffer;
     private int i;
     private float xSum;
     private float ySum;
     private float zSum;
     private Vector3 droping;
+    private Vector3[] smoothingBufferArray;
+    private float trianglurNumber;
 
     /// <summary>
     /// Set the hand collider tag.
@@ -48,6 +52,7 @@ public class HandCollider : MonoBehaviour
             ySum = 0;
             zSum = 0;
         }
+        trianglurNumber = smoothingFrame*(smoothingFrame+1)/2;
     }
 
     /// <summary>
@@ -57,15 +62,28 @@ public class HandCollider : MonoBehaviour
     {
         tracking = ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info;
         currentPosition = Camera.main.ViewportToWorldPoint(new Vector3(tracking.palm_center.x, tracking.palm_center.y, tracking.depth_estimation));
+        
         // transform.position = currentPosition;
-        smoothedPostion = SmoothingPostion(currentPosition);
+        if (movingAverage)
+        {
+            smoothedPostion = LinearWeightedMovingAverage(currentPosition);
+        }
+        else if (linearWeightedMovingAverage)
+        {
+            smoothedPostion = LinearWeightedMovingAverage(currentPosition);
+        }
+        else
+        {
+            smoothedPostion = currentPosition;
+        }
+        
         transform.position = smoothedPostion;
         Debug.Log("Both Before and After: (" + currentPosition.x.ToString()+", "+ currentPosition.y.ToString() +", "+ currentPosition.z.ToString() + "), (" + smoothedPostion.x.ToString()+", "+ smoothedPostion.y.ToString() +", "+ smoothedPostion.z.ToString() + ")");
         Debug.Log("Before Only: " +currentPosition.x.ToString()+", "+ currentPosition.y.ToString() +", "+ currentPosition.z.ToString() + ")");
         Debug.Log("After Only: " +smoothedPostion.x.ToString()+", "+ smoothedPostion.y.ToString() +", "+ smoothedPostion.z.ToString() + ")");
     }
     
-    Vector3 SmoothingPostion(Vector3 current)
+    Vector3 MovingAverage(Vector3 current)
     {
         droping = smoothingBuffer.Dequeue();
         xSum -= droping.x;
@@ -78,6 +96,26 @@ public class HandCollider : MonoBehaviour
         zSum += current.z;
 
         return new Vector3(xSum / smoothingFrame, ySum / smoothingFrame, zSum / smoothingFrame);
+    }
+
+    Vector3 LinearWeightedMovingAverage(Vector3 current)
+    {
+        droping = smoothingBuffer.Dequeue();
+        smoothingBuffer.Enqueue(current);
+
+        xSum = 0;
+        ySum = 0;
+        zSum = 0;
+        smoothingBufferArray = smoothingBuffer.ToArray();
+        
+        for (i = 0; i < smoothingFrame; i++)
+        {
+            xSum += smoothingBufferArray[i].x * i+1;
+            ySum += smoothingBufferArray[i].y * i+1;
+            zSum += smoothingBufferArray[i].z * i+1;
+        }
+
+        return new Vector3(xSum / trianglurNumber, ySum / trianglurNumber, zSum / trianglurNumber);
     }
 
     void OnTriggerEnter(Collider other)
