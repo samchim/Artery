ce0e8db673cdd4ccad6678de5136a1681e97bc81
Sam Chim <samchim@mindcluster.ai>
2021-04-12 21:13:07 +0800

    added Winning count down

---
+++ NewUnityProject/Assets/Scripts/Bolt/WinningCheckingBolt.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinningCheckingBolt : Bolt.EntityBehaviour<ICubeState>
+{
+    public GameObject hotZoneTrigger;
+    public GameObject[] handColliderList;
+
+    private bool allowWin;
+    private int countDownSecond;
+
+    private int hotZoneTriggerCount;
+    private int handColliderCount;
+    private bool runningCountDown = false;
+
+    private StartWinningCountDownEvent startWinningCountDownEvent = StartWinningCountDownEvent.Create();
+    private StopWinningCountDownEvent stopWinningCountDownEvent = StopWinningCountDownEvent.Create();
+
+    private WinningCountDownBolt _winningCountDownBolt;
+
+    void Start()
+    {
+        if (entity.IsOwner)
+        {
+            StartCoroutine(starting());
+            hotZoneTrigger = GameObject.FindGameObjectWithTag("HotZoneTrigger");
+        }
+    }
+
+    public override void SimulateOwner()
+    {
+        if (entity.IsOwner)
+        {
+            hotZoneTriggerCount = hotZoneTrigger.GetComponent<triggerHotZone>().getTouchedCount();
+            // handColliderCount = handCollider.GetComponent<HandCollider>().getCollidingsCount();
+            handColliderCount = 0;
+            handColliderList = GameObject.FindGameObjectsWithTag("Player");
+            foreach (GameObject handCollider in handColliderList)
+            {
+                handColliderCount += handCollider.GetComponent<HandColliderBolt>().getCollidingsCount();
+            }
+
+            Debug.Log("winningCountDown: allowWin = " + allowWin.ToString() + ", hotZoneTriggerCount = " + hotZoneTriggerCount.ToString() + ", handColliderCount = " + handColliderCount.ToString() + ", runningCountDown = " + ((runningCountDown != null) ? runningCountDown.ToString() : "null"));
+            if (allowWin && hotZoneTriggerCount == 0 && handColliderCount == 0)
+            {
+                if (runningCountDown == false)
+                {
+                    startWinningCountDownEvent.Send();
+                    runningCountDown = true;
+                }
+            }
+            else
+            {
+                if (runningCountDown == true)
+                {
+                    stopWinningCountDownEvent.Send();
+                    runningCountDown = false;
+                }
+            }
+        }
+    }
+
+    IEnumerator starting()
+    {
+        yield return new WaitForSeconds(1f);
+        allowWin = true;
+    }
+}
---
+++ NewUnityProject/Assets/Scripts/Bolt/WinningCountDownBolt.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Bolt;
+
+public class WinningCountDownBolt : GlobalEventListener
+{
+    public int winningConditionSecond; 
+    public Text winningCountDownDisplay;
+
+    private int countDownSecond;
+
+    private Coroutine runningCountDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        runningCountDown = null;
+        countDownSecond = winningConditionSecond;
+        winningCountDownDisplay.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public override void OnEvent(StartWinningCountDownEvent evnt)
+    {
+        countDownSecond = winningConditionSecond;
+        winningCountDownDisplay.gameObject.SetActive(true);
+        runningCountDown = StartCoroutine(countDown());
+    }
+
+    public override void OnEvent(StopWinningCountDownEvent evnt)
+    {
+        winningCountDownDisplay.gameObject.SetActive(false);
+        StopCoroutine(runningCountDown);
+        runningCountDown = null;
+    }
+
+    IEnumerator countDown()
+    {
+        Debug.Log("winningCountDown: IEnumerator countDown()"); 
+        while (countDownSecond > 0)
+        {
+            winningCountDownDisplay.text = countDownSecond.ToString();
+            yield return new WaitForSeconds(1f);
+            countDownSecond--;
+        }
+        winningCountDownDisplay.text = "YOU WIN!";
+    }
+}
